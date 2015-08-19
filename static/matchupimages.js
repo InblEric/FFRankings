115f0d1afa3f18d2b914a5126cb3e16d263bcbf2
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2015-08-19 12:52:17 -0500

    JQuery to load player pic

---
+++ static/matchupimages.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    var playerURL = "http://www.nfl.com/player/brucegradkowski/2495838/profile"
+    var img = $( "#player1" ).load( playerURL + " .player-photo" );
+});
+
