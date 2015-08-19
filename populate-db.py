88fff0955696f0d98df5632715aee1e828662f3e
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2015-08-19 15:49:01 -0500

    Added script that populated db with nfl players

---
+++ populate-db.py
@@ -0,0 +1,25 @@
+from ffrankings_app import app
+from ffrankings_app import db
+from ffrankings_app import Player
+import nflgame
+Player.query.delete()
+
+QBs = []
+RBs = []
+WRs = []
+TEs = []
+with app.app_context():
+    for p in nflgame.players.itervalues():
+        if p.position == 'QB':
+            player = Player(p.name, p.position, p.team)
+            db.session.add(player)
+        elif p.position == 'RB':
+            player = Player(p.name, p.position, p.team)
+            db.session.add(player)        
+        elif p.position == 'WR':
+            player = Player(p.name, p.position, p.team)
+            db.session.add(player)        
+        elif p.position == 'TE':
+            player = Player(p.name, p.position, p.team)
+            db.session.add(player)
+    db.session.commit()
