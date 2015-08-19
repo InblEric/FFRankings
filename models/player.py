cbf9d01a682fa47d23a1803d622bd6b37c1ae409
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2015-08-19 11:16:50 -0500

    Made player database model

---
+++ models/player.py
@@ -0,0 +1,23 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+#from ffrankings_app import app
+
+db = SQLAlchemy()
+
+# Create our database models
+class Player(db.Model):
+    __tablename__ = "players"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    position = db.Column(db.String(120))
+    team = db.Column(db.String(120))
+    elo = db.Column(db.String(127))
+    #flexElo = db.Column(db.String(127))
+
+    def __init__(self, name, position, team):
+        self.name = name
+        self.position = position
+        self.team = team
+        self.elo = "1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00,1500.00"
+
+    def __repr__(self):
+        return "" + str(self.name) + " " + str(self.position) + " " + str(self.team)
