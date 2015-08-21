f2cea68d4861fe829ebaa00e59e12abfd3e799f7
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2015-08-21 18:49:24 -0500

    Online db

--- ffrankings_app.py
+++ ffrankings_app.py
@@ -8,8 +8,8 @@ import random
 import sys
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 
 # later on
